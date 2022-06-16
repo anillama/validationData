4f1770ac9b45eb6b82fef3d45fca53c40c3f7e76
anillama <33677877+anillama@users.noreply.github.com>
2022-06-16 11:10:36 -0600

    first commit

--- src/com/data/dao/UserDao.java
+++ src/com/data/dao/UserDao.java
@@ -1,4 +1,9 @@
 package com.data.dao;
 
 public class UserDao {
+    public boolean connection() {
+        boolean checkConnection = false;
+        System.out.println("This will check for the connection");
+        return checkConnection;
+    }
 }
