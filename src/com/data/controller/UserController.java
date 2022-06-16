9ac50e0b89cdefe7598c0276bdc81c746bc9bbf9
anillama <33677877+anillama@users.noreply.github.com>
2022-06-16 11:14:39 -0600

    commit from new Branch

--- src/com/data/controller/UserController.java
+++ src/com/data/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.data.controller;
 
 public class UserController {
+    //New method
+    //Needs to be impliment
+    public String setData(){
+        System.out.println("setData");
+        return null;
+    }
 
 }
