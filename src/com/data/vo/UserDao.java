6decc182d4e61eff4cb1f9219fbc7f504bb956e4
anillama <33677877+anillama@users.noreply.github.com>
2022-06-16 10:58:46 -0600

    first commit

---
+++ src/com/data/vo/UserDao.java
@@ -0,0 +1,50 @@
+package com.data.vo;
+
+public class UserDao {
+
+    private String firstName;
+    private String lastName;
+    private String userAddre;
+    private String userPhone;
+    private String userPassword;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserAddre() {
+        return userAddre;
+    }
+
+    public void setUserAddre(String userAddre) {
+        this.userAddre = userAddre;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
