6572c6b348d9dae310f2d7d93444c86f785f1e7d
anillama <33677877+anillama@users.noreply.github.com>
2022-09-11 21:57:36 -0500

    new message

---
+++ src/com/data/controller/TestClass.java
@@ -0,0 +1,13 @@
+package com.data.controller;
+
+import java.util.concurrent.ExecutionException;
+
+public class TestClass {
+
+    public static void main(String [] args) throws ExecutionException, InterruptedException {
+        //This is sysout..
+        System.out.println("Hello World!");
+        int val = 45;
+
+    }
+}
