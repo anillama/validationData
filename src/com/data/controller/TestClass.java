8ef9c0576418f7ca1ac52c237332f661c4a41cae
anillama <33677877+anillama@users.noreply.github.com>
2022-11-09 11:46:46 -0600

    first testing it revert

--- src/com/data/controller/TestClass.java
+++ src/com/data/controller/TestClass.java
@@ -4,11 +4,22 @@ import java.util.concurrent.ExecutionException;
 
 public class TestClass {
 
+    public static boolean checkDate(String val) {
+        if(val == "duplicate") {
+            return true;
+        }
+        return false;
+    }
+
+
     public static void main(String [] args) throws ExecutionException, InterruptedException {
-        //This is sysout..
-        //New lines of code.. with the line
-        System.out.println("Hello World!");
-        int val = 45;
-        //Adding new commit line..
+        String dub = "duplicate";
+       if(!checkDate(dub)){
+           System.out.println("Not Dublicate");
+       }
+       else {
+           System.out.println("Dublicate");
+       }
+
     }
 }
